047d0098032ee65d6a3335a20ae9c31089c2f1cb
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:13:30 +0530

    Add files via upload

---
+++ num_swap.java
@@ -0,0 +1,23 @@
+class num_swap
+{
+public static void main(String[] args)
+{
+int a = 10;
+int b = 11;
+int c = 0;
+System.out.println("Before Swapping");
+System.out.println("a =" +a);
+System.out.println("b =" +b);
+c = b;
+b = a;
+a = c;
+System.out.println("After Swapping");
+System.out.println("a =" +a);
+System.out.println("b =" +b);
+}
+}
+
+
+
+
+
