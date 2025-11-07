e2fd18991d083aaba360c2f412b2afda30aafc55
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:09:10 +0530

    Add files via upload

---
+++ Pallindrom_Number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Pallindrom_Number
+{
+public static void main(String[] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.println("Enter the Number");
+int n = scn.nextInt();
+int temp = n;
+int reverse = 0;
+while(n > 0)
+{
+int rem = n%10;
+reverse = (reverse*10)+rem;
+n /= 10;
+}
+if(temp == reverse)
+{
+System.out.println("Pallindrom Number");
+}
+else
+{
+System.out.println("Not Pallindrom Number");
+}
+System.out.println(reverse);
+}
+}
