6b5a9260ee7df8e90455992a43b145187974ec42
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:02:39 +0530

    Add files via upload

---
+++ Sum_Number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Sum_digit
+{
+public static void main(String[] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.println("Enter the number");
+int n = scn.nextInt();
+int sum = 0;
+while(n > 0)
+{
+int m = n%10;
+sum += m;
+n = n/10;
+}
+System.out.println(sum);
+}
+}
