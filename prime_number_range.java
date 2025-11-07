22b1c5438922ca09ef3c6d376d0bc9667f94bdd6
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:25:48 +0530

    Add files via upload

---
+++ prime_number_range.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class prime_number_range
+{
+public static void main(String[] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.println("Enter the Number");
+int start = scn.nextInt();
+int end = scn.nextInt();
+for(int i = start;i<=end;i++)
+{
+if(i <= 1) continue;
+boolean flag = true;
+for(int j = 2;j<=i/2;j++)
+{
+if(i % j == 0)
+{
+flag = false;
+break;
+}
+}
+if(flag)
+{
+System.out.println(i);
+}
+}
+}
+}
